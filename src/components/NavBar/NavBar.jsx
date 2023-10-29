1bce030303e4e787ec21196b9ab86f6391310558
Nicolas Roldan <nicolasroldan128@gmail.com>
2023-10-29 13:52:57 -0300

    primer commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import CartWidget from "../CartWidget/CartWidget"
+
+
+const NavBar = () => {
+  return (
+    <>  
+        <div>NavBar</div>
+        <CartWidget />
+    </>
+  )
+}
+
+export default NavBar
